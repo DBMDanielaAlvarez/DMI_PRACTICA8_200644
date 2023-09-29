7bcb26aa38897b0ed0ff4c931e8600812edd6a8d
Daniela06112002 <84827002+Daniela06112002@users.noreply.github.com>
2023-09-28 23:41:23 -0600

    ADD PRACTICA8

---
+++ lib/media_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp_200644/common/HttpHandler.dart';
+import 'package:movieapp_200644/model/Media.dart';
+
+class MediaList extends StatefulWidget {
+  @override
+  _MediaListState createState() => new _MediaListState();
+}
+
+class _MediaListState extends State<MediaList> {
+  List<Media> _media = [];
+  @override
+  void initState() {
+    super.initState();
+    loadMovie();
+  }
+
+  void loadMovie() async {
+    var movies = await HttpHandler().fetchMovies();
+    setState(() {
+      _media.addAll(movies);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new ListView.builder(
+          itemCount: _media.length,
+          itemBuilder: (BuildContext context, int index) {
+            return new Column(
+              children: <Widget>[
+                new Image.network(_media[index].getPosterUrl())
+              ],
+            );
+          }),
+    );
+  }
+}
